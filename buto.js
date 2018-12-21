2e07e38a7d1d17a776319e239517ef41b441bb6d
Uwerajojo <josianeuwera23@gmail.com>
2018-12-21 15:30:54 +0200

    add if function in buto.js

--- buto.js
+++ buto.js
@@ -1,1 +1,28 @@
-var flavor = $("input:radio[name=ab]:checked").val();
+
+function check(){
+
+    var s1 = document.quiz.s1.value;
+    var s2 = document.quiz.s2.value;
+    var s3 = document.quiz.s3.value;
+    var count=0;
+  
+        if (s1 == "d") {
+          count+=25;
+        }
+        if (s2 == "b"){
+          count+=25;
+        }
+        if (s3 == "b"){
+          count+=25;
+        }
+        
+    document.getElementById("after_submit").style.visibility ="visible";
+  
+    document.getElementById("number_count").innerHTML = "You got " +count + "%.";
+  
+    $("#button").hide();
+    $("#quiz").hide();
+    $("#number_count").show();
+    event.preventDefault();
+  
+  };
